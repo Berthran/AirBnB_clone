0bfbb1c325d5bfce292c8d17ff9dc71ca1ba5705
Daniel Berthran <berthlearn@gmail.com>
2024-05-13 13:27:52 +0100

    modify console.py file

--- console.py
+++ console.py
@@ -6,3 +6,14 @@ A command line interpreter for manipulating objects/instances.
 import cmd
 import uuid
 from datetime import datetime
+
+class HBNBConsole(cmd.Cmd):
+    '''
+    Instance of a cmd session
+    '''
+
+    prompt = "(hbnb) "
+
+
+if __name__ == "__main__":
+    console().cmdloop()
