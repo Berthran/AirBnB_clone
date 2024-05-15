62fcd59da62488d9353f69b723856720ee50da91
Daniel Berthran <70752338+Berthran@users.noreply.github.com>
2024-05-15 02:19:03 +0100

    Add comments file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''
-This module handles the serialization and deserialization of instances
+This module handles the storage, serialization and deserialization of instances
 in the models package.
 
 Class:
-    FileStorage: serializes instances of instances to JSON file
-    and deserializes JSON file to instances
+    FileStorage: stores instances to a file, serializes instances to a JSON file
+    and deserializes a JSON file to instances
 '''
 
 import json
@@ -15,18 +15,18 @@ from models.to_inst_attr import to_instantiable_attributes
 
 class FileStorage():
     '''
-    Serializes instances of instances to JSON file and deserializes
+    Stores, serializes instances to JSON file and deserializes
     JSON file to instances
 
     Class attributes:
-        file_path: string - path to the JSON file (private class attribute)
-        objects: a dictionary used to store all objects with the key
-                 <class_name.id> (private class attribute)
+        file_path: string - path to the JSON file : private class attribute
+        objects: a dictionary used to store all instances created with the unique key
+                 <class_name.id> : private class attribute
     Methods:
         all: returns the <objects> attribute
         new: add new instances to the <objects> attribute
-        save: serializes the <objects> attribute to the JSON file in <file_path>
-        reload: deserializes the JSON file to <objects> if the JSON file exists.
+        save: serializes the instances in the <objects> attribute and stores them in the JSON file specified in <file_path>
+        reload: deserializes the JSON file to dictionary objects, creates instances and loads the instances into the <objects> attribute (only if the JSON file exists).
     '''
 
     __file_path = "file.json"
@@ -46,21 +46,24 @@ class FileStorage():
         FileStorage.__objects.update({instance_storage_id: obj})
 
     def save(self):
-        '''Serializes the <objects> attributes to JSON file'''
-        # Convert <object> to JSON string
+        '''Serializes the instances in the <objects> attributes and stores them to the JSON file'''
+        # A container to store the serialized instances
         instance_storage = {}
         for instance_id, instance in FileStorage.__objects.items():
+            # Modifying the instances serialization-unfriendly attributes using to_dict() method and
+            # storing the modified attributes in the container created above using their instance ID
             instance_storage[instance_id] = instance.to_dict()
+        # Serializing the instances to JSON string
         instance_storage_in_JSON = json.dumps(instance_storage)
-        # Write the JSON string of <objects> to file.json
+        # Writing the JSON string to file.json
         with open(FileStorage.__file_path, "w") as json_file:
             json_file.write(instance_storage_in_JSON)
 
     def reload(self):
-        '''Deserializes the JSON file, creates instance and adds instance to
-        <objects>'''
+        '''Deserializes the JSON file, creates instances and loads each instance into the
+        <objects> attribute'''
         try:
-            # Opening file only if it exists
+            # Open file.json only if it exists
             with open(FileStorage.__file_path, "r") as json_file:
                 # Extracting JSON string from file
                 objectsInStringFormat = json_file.read()
@@ -74,16 +77,9 @@ class FileStorage():
                     # suitable for instantiation
                     instance_attributes = \
                             to_instantiable_attributes(**inst_dict)
-                    # Create instance based on class name
+                    # Create an instance based on the class name
                     baseModel = BaseModel(**instance_attributes)
                     # Add instance to <objects> attribute
                     self.new(baseModel)
         except Exception:
             pass
-
-
-
-
-
-
-
