d138855fc426318f380fc5c38e2cb2959e31f30b
Daniel Berthran <berthlearn@gmail.com>
2024-05-15 01:45:11 +0100

    Write all, new, save and reload method for BaseClass

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,10 @@ Class:
     and deserializes JSON file to instances
 '''
 
+import json
+from models.base_model import BaseModel
+from models.to_inst_attr import to_instantiable_attributes
+
 
 class FileStorage():
     '''
@@ -25,4 +29,61 @@ class FileStorage():
         reload: deserializes the JSON file to <objects> if the JSON file exists.
     '''
 
-    __file_path
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''Returns the <objects> attributes'''
+        return (FileStorage.__objects)
+
+    def new(self, obj):
+        '''Add an instance/object to the <objects> attribute with a unique ID'''
+        # Create a unique instance id using class name and instance id
+        class_name = obj.__class__.__name__
+        instance_id = obj.id
+        instance_storage_id = class_name + "." + instance_id
+        # Add instance to <objects> attribute
+        FileStorage.__objects.update({instance_storage_id: obj})
+
+    def save(self):
+        '''Serializes the <objects> attributes to JSON file'''
+        # Convert <object> to JSON string
+        instance_storage = {}
+        for instance_id, instance in FileStorage.__objects.items():
+            instance_storage[instance_id] = instance.to_dict()
+        instance_storage_in_JSON = json.dumps(instance_storage)
+        # Write the JSON string of <objects> to file.json
+        with open(FileStorage.__file_path, "w") as json_file:
+            json_file.write(instance_storage_in_JSON)
+
+    def reload(self):
+        '''Deserializes the JSON file, creates instance and adds instance to
+        <objects>'''
+        try:
+            # Opening file only if it exists
+            with open(FileStorage.__file_path, "r") as json_file:
+                # Extracting JSON string from file
+                objectsInStringFormat = json_file.read()
+                # Deserializing JSON string to dict type
+                objectsInDictForm = json.loads((objectsInStringFormat))
+                # Creating an instance for each instance data in the file
+                for inst_dict in objectsInDictForm.values():
+                    # Get the name of the class each instance data belongs to
+                    class_name = inst_dict["__class__"]
+                    # Modify the data to remove and convert necessary data
+                    # suitable for instantiation
+                    instance_attributes = \
+                            to_instantiable_attributes(**inst_dict)
+                    # Create instance based on class name
+                    baseModel = BaseModel(**instance_attributes)
+                    # Add instance to <objects> attribute
+                    self.new(baseModel)
+        except Exception:
+            pass
+
+
+
+
+
+
+
