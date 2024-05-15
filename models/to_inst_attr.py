6aaa86386a9a0961882dd8a2cd0e09c347869c12
Daniel Berthran <70752338+Berthran@users.noreply.github.com>
2024-05-15 02:04:56 +0100

    Add comments to_inst_attr.py

--- models/to_inst_attr.py
+++ models/to_inst_attr.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 '''
-Modifies the attributes of an instance stored in a dictionary object,
-to one that can be used to create an instance of the object.
+Modifies the attributes (key/value pair) in a dictionary object,
+to one that can be used to create an instance of a specified type.
+
+Functions:
+    to_instantiable_attributes: takes the attributes and suit them up for instantiation
 '''
 
 
@@ -9,19 +12,23 @@ from datetime import datetime
 
 
 def to_instantiable_attributes(**kwargs):
-    '''Modifies the kwargs to be able create an instance'''
-    # Remove the "__class__" attribute
+    '''Modifies the keyword arguments to be able to create an instance with them
+    Returns the modified keyword arguments
+    '''
+    # STEP 1: Removes the "__class__" attribute
     if "__class__" in kwargs.keys():
         kwargs.pop("__class__")
-    # Change "created_at" attribute from string format to datetime
+    # STEP 2: Changes the "created_at" attribute from a string format to a datetime format
     if "created_at" in kwargs.keys():
+        # Error handling in case the format is already changed to a datetime format before calling this function
         if (type(kwargs['created_at']) is str):
             str_format_of_created_at = kwargs["created_at"]
             datetime_format_of_created_at = \
                     datetime.fromisoformat(str_format_of_created_at)
             kwargs["created_at"] = datetime_format_of_created_at
-    # Change "updated_at" attribute from string format to datetime
+    # STEP 3: Change the "updated_at" attribute from string format to a datetime format
     if "updated_at" in kwargs.keys():
+        # Error handling in case the format is already changed to a datetime format before calling this function
         if (type(kwargs['updated_at']) is str):
             str_format_of_updated_at = kwargs["updated_at"]
             datetime_format_of_updated_at = \
