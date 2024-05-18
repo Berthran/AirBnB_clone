d0e75891837d31cba985cbddc197c5106f8b26e7
Daniel Berthran <berthlearn@gmail.com>
2024-05-18 19:14:43 +0100

    pycodestyle fixes

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ Classes:
 
 import uuid
 import models
-from models.to_inst_attr import to_instantiable_attributes
+from models.to_inst_attr import modifyKwargsForInstantiation
 from datetime import datetime
 
 
@@ -19,9 +19,10 @@ class BaseModel():
     for other classes in the AirBnB project.
 
     Methods:
-        save: updates the `updated_at` instance attribute with the current datetime
-        to_dict: returns a dictionary containing all keys/values of the __dict__
-        attribute of the instance
+        save: updates the `updated_at` instance attribute with the current
+                datetime
+        to_dict: returns a dictionary containing all keys/values of the
+                __dict__ attribute of the instance
     '''
 
     def __init__(self, *args, **kwargs):
@@ -38,22 +39,23 @@ class BaseModel():
         if (numberOfItemsInKwargs == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at # (To be updated every time an
+            self.updated_at = self.created_at  # (To be updated every time an
             # instance is saved)
             # Add the instance to a storage for instances
             models.storage.new(self)
         else:
-            # If kwargs are used, modify the attributes to suit instantiation
-            kwargs = to_instantiable_attributes(**kwargs)
+            kwargsForInstantiation = modifyKwargsForInstantiation(**kwargs)
             # Initialize instance with the modified attributes in kwargs
-            for attribute, value in kwargs.items():
-                # Add the attributes and their values to the __dict__ attribute of the instance
-                # here by instantiating it
+            for attribute, value in kwargsForInstantiation.items():
+                # Add the attributes and their values to the __dict__
+                # attribute of the instance here by instantiating it
                 self.__dict__[attribute] = value
 
-
     def __str__(self):
-        ''' Displays a human-readable string representation of the BaseModel instance '''
+        '''
+        Displays a human-readable string representation of the
+        BaseModel instance
+        '''
         className = self.__class__.__name__
         instanceId = self.id
         instanceAttributes = self.__dict__
@@ -65,17 +67,21 @@ class BaseModel():
         self.updated_at = datetime.now()
         # Call function to store/persist instance to a file
         models.storage.save()
-        
+
     def to_dict(self):
         ''' Creates a modified dictionary object of the instance attributes
         suitable for storing to a file'''
-        instanceAttributes = self.__dict__.copy() # Prevents changes to __dict__ object
-        # Add a __class__ attribute to the instance attributes, with the class name as it's value
-        instanceAttributes['__class__'] = self.__class__.__name__
+        # Prevents changes to __dict__ object
+        instanceAttributes = self.__dict__.copy()
+        # Add a __class__ attribute to the instance attributes,
+        # with the class name as it's value
+        instanceAttributes.update('__class__'] = self.__class__.__name__
         # Change the created_at attribute to string format (isoformat)
         if (type(instanceAttributes.get("created_at")) is datetime):
-            instanceAttributes['created_at'] = datetime.isoformat(self.created_at)
+            instanceAttributes['created_at'] = \
+                    datetime.isoformat(self.created_at)
         # Change the updated_at attribute to string format (isoformat)
         if (type(instanceAttributes.get("updated_at")) is datetime):
-            instanceAttributes['updated_at'] = datetime.isoformat(self.updated_at)
+            instanceAttributes['updated_at'] = \
+                    datetime.isoformat(self.updated_at)
         return (instanceAttributes)
