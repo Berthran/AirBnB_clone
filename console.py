4b9e2d3b367ba36ffb901d9246b6ca05a3526f91
Daniel Berthran <berthlearn@gmail.com>
2024-05-19 07:43:19 +0100

    Add emptyline() method to handle empty line commands.
    Do nothing for an emptyline command.

--- console.py
+++ console.py
@@ -6,6 +6,10 @@ A command line interpreter for manipulating objects/instances.
 import cmd
 import uuid
 from datetime import datetime
+from models.base_model import BaseModel
+from console_tools import isValidClassNameArgument
+from console_tools import getAndCreateClassInstance
+from console_tools import saveClassInstanceToJSONFile
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,10 +31,22 @@ class HBNBCommand(cmd.Cmd):
         '''Provide bried description of available commands'''
         cmd.Cmd.do_help(self, arg)
 
-    def emptyline(sef):
+    def emptyline(self):
         '''Handle empty line commands'''
         pass
 
+    def do_create(self, classNameArgument):
+        '''Creates a new instance of the class name argument
+        and saves it to a JSON file'''
+        empty = ""
+        if classNameArgument == empty:
+            print("** class name missing **")
+        else:
+            if(isValidClassNameArgument(classNameArgument)):
+                newClassInstance = getAndCreateClassInstance(classNameArgument)
+                saveClassInstanceToJSONFile(newClassInstance)
+            else:
+                print("** class doesn't exist **")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
