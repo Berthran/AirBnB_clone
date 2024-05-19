2917e53b59273b071fffd932770eeb00c6c5946b
Daniel Berthran <berthlearn@gmail.com>
2024-05-19 06:55:38 +0100

    create a module for useful console funtions

---
+++ console_tools.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+This module contains some useful functions for the HBNBCommand Class.
+
+It includes utilities for creating an instance, displaying an instance,
+destroying an instance, displaying multiple instances and updating instances.
+
+'''
+
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+def isValidClassNameArgument(classNameArgument, nameSpaceOfModule):
+    '''Checks the argument(s) in classNameArgument'''
+    noOfArgsInClassNameArgument = len(classNameArgument.split())
+    if (noOfArgsInClassNameArgument != 1):
+        print("args count fail")
+        return (0)
+    if (classNameArgument not in nameSpaceOfModule):
+        print("class name fail")
+        print(nameSpaceOfModule)
+        return (0)
+    return (1)
+
+def getAndCreateClassInstance(classNameArgument):
+    '''Retrieves the class object from the class name and
+    creates an instance of the class'''
+    availableClasses = {"BaseModel": BaseModel(),
+                        "FileStorage": FileStorage()}
+    for className, classInstance in availableClasses.items():
+        if className == classNameArgument:
+            return (classInstance)
+
+def saveClassInstanceToJSONFile(classInstance):
+    '''Saves the class instance to a JSON file and prints its id'''
+    classInstance.save()
+    print(classInstance.id)
